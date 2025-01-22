3f3ab09a916f75cecda89c13d0d2ae55d24a5edd
ayanpandey27 <ayanroyal1174@gmail.com>
2025-01-22 14:40:03 +0530

    Mark

---
+++ AverageMark.java
@@ -0,0 +1,15 @@
+public class AverageMark {
+    public static void main(String[] args) {
+        // Sam's marks in PCM
+        int mathsMark = 94;
+        int physicsMark = 95;
+        int chemistryMark = 96;
+
+        // Calculate the average
+        double average = (mathsMark + physicsMark + chemistryMark) / 3.0;
+
+        // Output the result
+        System.out.printf("Sam's average mark in PCM is %.2f", average);
+    }
+}
+
